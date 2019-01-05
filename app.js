e8bacaa8581615cfd5c6c7236a58512ff162dfe1
Treasure <treasure@Admins-Mac-Pro-2.local>
2019-01-05 12:35:39 -0800

    s2v1

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/quotes', (req, res)=>{
-    res.json({greeting: "Hello World!"});
-});
-
 // Send a GET request to /quotes to READ a list of quotes
+app.get('/quotes', (req, res)=>{
+    res.json(data);
+});
 // Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', (req, res)=>{
+    const quote = data.quotes.find(quote => quote.id == req.params.id);
+    res.json(quote);
+});
 // Send a POST request to /quotes to  CREATE a new quote 
 // Send a PUT request to /quotes/:id to UPDATE (edit) a quote
 // Send a DELETE request to /quotes/:id DELETE a quote 
